77c9bed0e7829e4167aea146a976cd15b9b94dfb
beattycharles <113803621+beattycharles@users.noreply.github.com>
2022-11-06 14:48:56 -0500

    Readme added

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Quiz
+
+## Table of Contents
+[Description](#description)<br>
+[Useage](#usage)<br>
+[Screeshot](#screeshot)<br>
+[Access](#access)<br>
+[Credits](#credits)<br>
+[Roadmap](#roadmap)<br>
+[License](#license)
+
+## Description
+A timed quiz of the movie Monty Python and the Holy Grail.
+
+## Usage
+Created to show working knowledge of  dom traveral in javascript.
+
+## Screeshot
+![Screenshot](HolyG.png)
+
+## Access
+
+[Github](https://www.github.com/beattycharles)
+[Quiz](https://beattycharles.github.io/Quiz/)
+
+## Credits
+Special Thanks to: https://github.com/BogartDME https://github.com/EvolanGIT https://github.com/difurung.
+W3school for information on JavaScript.
+Metacritic.com for Grail background
+media.tenor.com for Gorge of death gif
+
+
+## Roadmap
+As of 11/6/22 need to have timer start with the question, also have gif stay on game over page.
+
+## License
+Copyright (c) 2022 beattycharles
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assits/script.js
+++ assits/script.js
@@ -15,38 +15,36 @@ var btnTryAgain = scoreboardPageEl.querySelector("#try-again");
 var score = 0;
 var savedScoresArr = [];
 var scoreboard;
+var clearInterval;
 var myQuestions = [
   {
     question: "What is your name?",
     answersChoices: [
-    "Tim",
-    "Author King of the Britians",
-    "Knights who say NI."],
-    answerCorrect: "Author King of the Britians"
-    },
-    {
-      question: "What is my favorite Color?",
-      answersChoices: [
-        "Red",
-        "Blue",
-        "Green",],
-        answerCorrect: "Blue"
-      },
-      {
-        question: "What is the air speed velocity of an unladen swallow?",
-        answersChoices: [
-        "24 mph",
-        "32kph",
-        "African or European?",],
-        answerCorrect: "African or European?"
-        },
-        {
-          question: "What holy relic does Brother Maynard carry with him?",
-          answersChoices: [
-          "Holy Hand Grenade of Antioch",
-          "Book of Armaments",
-          "The Jaw of St Anthony",],
-          answerCorrect: "Holy Hand Grenade of Antioch"},
+      "Tim",
+      "Author King of the Britians",
+      "Knights who say NI.",
+    ],
+    answerCorrect: "Author King of the Britians",
+  },
+  {
+    question: "What is my favorite Color?",
+    answersChoices: ["Red", "Blue", "Green"],
+    answerCorrect: "Blue",
+  },
+  {
+    question: "What is the air speed velocity of an unladen swallow?",
+    answersChoices: ["24 mph", "32kph", "African or European?"],
+    answerCorrect: "African or European?",
+  },
+  {
+    question: "What holy relic does Brother Maynard carry with him?",
+    answersChoices: [
+      "Holy Hand Grenade of Antioch",
+      "Book of Armaments",
+      "The Jaw of St Anthony",
+    ],
+    answerCorrect: "Holy Hand Grenade of Antioch",
+  },
 ];
 gameOverEl.style.display = "none";
 scoreboardPageEl.style.display = "none";
@@ -54,24 +52,23 @@ scoreboardPageEl.style.display = "none";
 function setTime() {
   hideText.textContent = "";
   hideBar.textContent = "";
-  Canvas.remove();
+  // Canvas.remove();
   showQuestion();
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + "seconds left";
-    
+
     if (secondsLeft <= 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       sendMessage();
     }
   }, 1000);
-  
-};
+}
 
-var showQuestion = function(){
+var showQuestion = function () {
   questionHeading.textContent = myQuestions[qIndex].question;
-  
+
   for (var i = 0; i < 4; i++) {
     var answerChoice = document.createElement("p");
     answerChoice.textContent = myQuestions[qIndex].answersChoices[i];
@@ -83,30 +80,29 @@ var showQuestion = function(){
 };
 
 var checkAnswer = function (event) {
-  
   // Check for correct answer
   var chosenAnswer = event.target;
-  if (chosenAnswer.textContent === myQuestions[qIndex].answerCorrect) 
-  { score = score + 5;
-  }
-  else {
+  if (chosenAnswer.textContent === myQuestions[qIndex].answerCorrect) {
+    score = score + 5;
+  } else {
     secondsLeft = secondsLeft - 10;
   }
-  qIndex++
-  
+  qIndex++;
+
   if (qIndex < myQuestions.length) {
     while (answersEl.firstChild) {
-    questionHeading.textContent = "";
-    answersEl.removeChild(answersEl.firstChild);
-    };
-    
+      questionHeading.textContent = "";
+      answersEl.removeChild(answersEl.firstChild);
+    }
+
     showQuestion();
   } else {
     sendMessage();
     return;
-  };
+  }
+  clearInterval(timerInterval);
 };
-var sendMessage = function() {
+var sendMessage = function () {
   questionHeading.remove();
   questionPageEl.remove();
   timeEl.textContent = " ";
@@ -114,7 +110,7 @@ var sendMessage = function() {
   score = score + secondsLeft;
   var imgEl = document.createElement("img");
   imgEl.setAttribute("src", "./assits/monty-python-holy-grail.gif");
-  imgEl.onload = function () {    
+  imgEl.onload = function () {
     this.style.position = "absolute";
   };
   timeEl.appendChild(imgEl);
@@ -122,38 +118,39 @@ var sendMessage = function() {
 
 var gameOver = function () {
   score = score + timeLeft;
-  
+  clearInterval(timerInterval);
+
   // Display current score
   var displayScoreEl = gameOverEl.querySelector("#your-score");
   displayScoreEl.textContent = "YOUR SCORE: " + score;
-  
+
   return score;
 };
 
 // Save score
 var submitScore = function (event) {
   event.preventDefault();
-  
+
   var initialsSave = gameOverEl.querySelector("#initials").value;
-  
+
   scoreboard = JSON.parse(localStorage.getItem("score")) || [];
   // savedScoresArr.push(scoreboard);
   // console.log(savedScoresArr);
-  
+
   // Save initial and score pair as an object and push to savedScoresArr
   var scoreObj = {
     initial: initialsSave,
-    score: score
+    score: score,
   };
   console.log(scoreObj);
   scoreboard.push(scoreObj);
-  
+
   // Stringify array for local storage
   localStorage.setItem("score", JSON.stringify(scoreboard));
-  
+
   gameOverEl.style.display = "none";
   scoreboardPageEl.style.display = "block";
-  
+
   loadScore();
 };
 
@@ -162,10 +159,10 @@ var loadScore = function () {
   if (!savedScoresArr) {
     return false;
   }
-  
+
   var scoreTableBody = scoreboardPageEl.querySelector("#score-table-body");
   scoreboard = JSON.parse(localStorage.getItem("score")) || [];
-  
+
   //create table row per each saved score object
   for (var i = 0; i < scoreboard.length; i++) {
     var scoreTableRow = document.createElement("tr");
